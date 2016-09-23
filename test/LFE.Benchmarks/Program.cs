8d16382da6ae0c03390d3e0aaf97bdc72ee3420f
0xffAA <akulyakov.artem@gmail.com>
2016-09-23 17:45:29 +0300

    benchmark was added
    simple benchmark was added(used BenchmarkDotNet)

---
+++ test/LFE.Benchmarks/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+namespace LFE.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BenchmarkRunner.Run<CommonBenchmark>(
+                    ManualConfig
+                        .Create(DefaultConfig.Instance)
+                    );
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
